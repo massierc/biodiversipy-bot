91088e7b3c586523a0a804699b036c7202a93ae7
Massimo Ercolani <massimo.ercolani28@gmail.com>
2022-06-07 15:01:49 +0200

    polling setup

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 import os
-import pandas as pd
-import telegram
+import logging
 
-from google.cloud import bigquery
-from operator import itemgetter as g
-from predictor import Predictor
+from handlers import register_handlers
+from queue import Queue
 
-client = bigquery.Client()
+from telegram.ext import Updater, Dispatcher
+from telegram import Bot, Update
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
 
 def biodiversipy_bot(request):
-    bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
+    bot = Bot(os.environ["TELEGRAM_TOKEN"])
+    update_queue = Queue()
+    dispatcher = Dispatcher(bot, update_queue)
+    register_handlers(dispatcher)
 
     if request.method == "POST":
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-        chat_id = update.message.chat.id
-
-        location = "TODO extract location from chat"
-
-        predictor = Predictor(location, client)
-        predictor.predict()
-
-        bot.sendMessage(chat_id=chat_id, text=predictor.predictions_text)
+        json = request.get_json(force=True)
+        update = Update.de_json(json, bot)
+        dispatcher.process_update(update)
 
     return "ok"
+
+
+def main():
+    bot = Bot(os.environ["TELEGRAM_TOKEN"])
+    updater = Updater(bot=bot, use_context=True)
+    register_handlers(updater.dispatcher)
+    updater.start_polling()
+
+
+if __name__ == "__main__":
+    main()
--- predictor.py
+++ predictor.py
@@ -1,16 +1,18 @@
+from google.cloud import bigquery
 from utils import get_features
 
-class Predictor():
-    def __init__(self, location, client):
-        self.client = client
+
+class Predictor:
+    def __init__(self, location):
+        self.client = bigquery.Client()
         self.features = get_features(location, self.client)
 
     def predict(self):
         self.predictions = [
-            ('Some species', 0.23),
-            ('Some other species', 0.15),
-            ('A third species', 0.04),
+            ("Some species", 0.23),
+            ("Some other species", 0.15),
+            ("A third species", 0.04),
         ]
-        self.predictions_text = "\n".join([
-            f"{species[0]} - {species[1]:.2%}"
-            for species in self.predictions])
+        self.predictions_text = "\n".join(
+            [f"{species[0]} - {species[1]:.2%}" for species in self.predictions]
+        )
